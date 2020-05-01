8c75916c57b5d7caebbcff4d5f9064a2f4eae36b
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-05-01 09:02:57 -0400

    structure for animation frame, spaces in js document for different aspects

--- app/static/js/sim.js
+++ app/static/js/sim.js
@@ -4,3 +4,50 @@
 // Virus Spread Simulation
 
 console.log("sim.js connected");
+
+	// =========================
+	// ELEMENT SELECTORS
+	// =========================
+
+
+const startButton = document.getElementById("start-button");
+const svg = document.getElementById("sim-svg");
+const sampleHuman = document.getElementById("sample-human");
+
+	// =========================
+	// GLOBAL VARIABLES
+	// =========================
+
+let id = 0;
+
+	// =========================
+	// SIMULATION FUNCTIONS
+	// =========================
+
+const step = function(timestamp) {
+	if(id){
+		console.log("new step");
+
+
+		id = window.requestAnimationFrame(step);
+	}else{
+		console.log("step called without an active frame?");
+	}
+}
+	// =========================
+	// EVENT LISTENER FUNCTIONS
+	// =========================
+	
+const start = function(e) {
+	e.preventDefault();
+	if ( !id ){
+		id = window.requestAnimationFrame(step);
+	}
+}
+
+	// =========================
+	// ATTACH EVENT LISTENERS
+	// =========================
+
+
+startButton.addEventListener('click',start);
