b4d3b437739747360f0d6174036b29ec95dc5b69
Lauren <lpehlivanian00@stuy.edu>
2020-05-04 00:12:54 -0400

    added code to get complete array of dictionary for daily global counts

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,51 @@
 #prepares flask
 from flask import Flask
+import urllib, json
 app = Flask(__name__) #create instance of class Flask
 
 #normal route
 @app.route("/") #assign following fxn to run when root route requested
 def helloworld():
     print(__name__) #prints in terminal
+    get_globalCases()
+    #get_globalCount()
     return "Hello, World." #prints on webpage
+
+#fetch global confirmed/death/recovery data (json)
+def get_globalCases():
+    global_cases = [] #array storing dicts {'date': 123123, 'confirmed', 'deaths', 'recovered'}
+    #read from api
+    url = "https://pomber.github.io/covid19/timeseries.json"
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+
+    #global_cases = data["Afghanistan"]
+
+    #print(data["Afghanistan"][0])
+    for country in data:
+        for date in data[country]:
+            added = False
+            for stored_date in global_cases:
+                #print(stored_date)
+                #print(date)
+                if stored_date["date"] == date["date"]:
+                    added = True #no need to add this date as new dict to global_cases
+                    stored_date["confirmed"] += date["confirmed"]
+                    stored_date["deaths"] += date["deaths"]
+                    stored_date["recovered"] += date["recovered"]
+            if not added:
+                global_cases.append(date)
+    print(global_cases[0:10])
+    return global_cases
+
+
+def get_globalCount():
+    url = "https://api.covid19api.com/summary"
+    response = urllib.urlopen(url)
+    data = json.loads(response.read())
+
+    print(type(data))
+    return data
 
 #main
 if __name__ == "__main__":
