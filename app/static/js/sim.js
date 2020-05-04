15b27fd9a464735822d26ef454eb6a8b59b59585
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-05-04 07:37:49 -0400

    working setup, generating dots to rep people

--- app/static/js/sim.js
+++ app/static/js/sim.js
@@ -13,35 +13,67 @@ console.log("sim.js connected");
 const startButton = document.getElementById("start-button");
 const svg = document.getElementById("sim-svg");
 const sampleHuman = document.getElementById("sample-human");
-
+svg.removeChild(sampleHuman);
 	// =========================
 	// GLOBAL VARIABLES
 	// =========================
 
 let id = 0;
+let setupComplete = false;
+
+let countHumans = 255;
 
 	// =========================
 	// SIMULATION FUNCTIONS
 	// =========================
 
+const wiggle = function(human_elt){
+	
+}
+
 const step = function(timestamp) {
 	if(id){
 		console.log("new step");
-
-
+		let humans = svg.getElementsByClassName("circle");
+		for( human in humans ) wiggle(human);
+		
 		id = window.requestAnimationFrame(step);
 	}else{
 		console.log("step called without an active frame?");
+	}
+}
+
+const setup = function(timestamp) {
+	if(id){
+		console.log("setting up");
+		for(let i = 0; i < countHumans; i++){
+			let human = sampleHuman.cloneNode();
+			human.removeAttribute('display');
+			human.setAttribute('cx',Math.random()*500);
+			human.setAttribute('cy',Math.random()*500);
+			svg.appendChild(human);
+		}
+		id = window.requestAnimationFrame(step);
+		setupComplete = true;
+	}else{
+		console.log("setup called without an active frame");
 	}
 }
 	// =========================
 	// EVENT LISTENER FUNCTIONS
 	// =========================
 	
-const start = function(e) {
+const start_stop = function(e) {
 	e.preventDefault();
 	if ( !id ){
-		id = window.requestAnimationFrame(step);
+		const callback = setupComplete ? step : setup;
+		id = window.requestAnimationFrame(callback);
+		e.target.textContent = "Stop";
+		console.log(e.target.textContent);
+	}else{
+		window.cancelAnimationFrame(id);
+		id = 0;
+		e.target.textContent = "Start";
 	}
 }
 
@@ -50,4 +82,4 @@ const start = function(e) {
 	// =========================
 
 
-startButton.addEventListener('click',start);
+startButton.addEventListener('click',start_stop);
