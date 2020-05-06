12b2d913e969081a34a505e88b2a4bcbdeb6bf37
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-05-06 08:37:18 -0400

    world wrapping, structure of a go function

--- app/static/js/sim.js
+++ app/static/js/sim.js
@@ -34,11 +34,29 @@ const wiggle = function(human){
 	let heading = parseFloat(human.getAttribute('heading')) + d_heading;
 	let dx = Math.cos(heading) * 5;
 	let dy = Math.sin(heading) * 5;
-	let cx = parseInt(human.getAttribute('cx')) + dx;
-	let cy = parseInt(human.getAttribute('cy')) + dy;
+	let cx = parseFloat(human.getAttribute('cx')) + dx;
+	let cy = parseFloat(human.getAttribute('cy')) + dy;
 	human.setAttribute('cx',cx);
 	human.setAttribute('cy',cy);
 	human.setAttribute('heading',heading);
+}
+
+const worldWrap = function(human){
+	let cx = parseFloat(human.getAttribute('cx'));
+	let cy = parseFloat(human.getAttribute('cy'));
+
+	if(cx > 500) cx = cx - 500;
+	if(cx < 0)   cx = cx + 500;
+	if(cy > 500) cy = cy - 500;
+	if(cy < 0)   cy = cy + 500;
+
+	human.setAttribute('cx',cx);
+	human.setAttribute('cy',cy);
+}
+
+const go = function(human){
+	wiggle(human);
+	worldWrap(human);
 }
 
 const step = function(timestamp) {
@@ -46,7 +64,7 @@ const step = function(timestamp) {
 		console.log("new step");
 		for(let i = 0; i < countHumans; i++){
 			// console.log ( svg.children.item(i) );
-			wiggle(svg.children.item(i));
+			go(svg.children.item(i));
 		}
 		
 		id = window.requestAnimationFrame(step);
