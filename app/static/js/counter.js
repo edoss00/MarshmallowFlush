d6b3c36f5f507bd2ca91c259487101fe5fbffd4e
Lauren <lpehlivanian00@stuy.edu>
2020-05-08 09:08:40 -0400

    finished counter w toggling numbers

--- app/static/js/counter.js
+++ app/static/js/counter.js
@@ -1,32 +1,47 @@
 console.log("counter here!");
-//get count:
+
+//document variables
 const script_tag = document.getElementById("counter-script");
+const counter = document.getElementById("counter");
+//create title div to display counter title
+const title = document.createElement('div');
+title.id = "title";
+counter.appendChild(title);
+title.innerHTML = "Global Counter";
+//create count div to display actual numbers
+const count_text = document.createElement('div');
+count_text.id = "count_text";
+counter.appendChild(count_text);
+
+//get count:
 var total = script_tag.getAttribute("count");
 
-const counter = document.getElementById("counter");
 
 //counter div style:
 counter.style.border = "1px solid blue";
 counter.style.margin = "0 auto";
-counter.style["max-width"] = "300px";
+counter.style["max-width"] = "400px";
 
-//counter div title text:
-var title = document.createTextNode("Global Counter");
-var br = document.createElement("br");
-counter.appendChild(title);
-counter.appendChild(br);
+//counter div text style:
 counter.style.fontSize = "200%";
 counter.style.textAlign = "center";
-//counter div count text
-var count = document.createTextNode(total);
-counter.appendChild(count);
+title.style.color = "red";
+count_text.style.fontSize = "300%";
+var timer = 0;
 
-const count_up = function(total) {
-  var i = 0;
-  while (i<100) {
-    count = ""+i;
-    counter.appendChild(count);
+const count_up = function() {
+  console.log(cur, parseInt(total));
+  count_text.innerHTML = cur;
+  cur += add;
+  timer += 10;
+  if (cur >= total) {
+      console.log("done");
+      clearInterval(step);
+      count_text.innerHTML = total;
   }
 }
 
-count_up(total);
+//timer for count_up function (executes over 3 secs)
+var step = setInterval(count_up, 10);
+var add = Math.floor(parseInt(total) / 300);
+var cur = 0;
