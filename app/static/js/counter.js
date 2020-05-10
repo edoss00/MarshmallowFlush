432019c316a396d3c2a4750cf6eae2980a4fa182
Lauren <lpehlivanian00@stuy.edu>
2020-05-10 10:33:08 -0400

    changed line and text color for theme

--- app/static/js/counter.js
+++ app/static/js/counter.js
@@ -3,29 +3,33 @@ console.log("counter here!");
 //document variables
 const script_tag = document.getElementById("counter-script");
 const counter = document.getElementById("counter");
-//create title div to display counter title
-const title = document.createElement('div');
-title.id = "title";
-counter.appendChild(title);
-title.innerHTML = "Global Counter";
+
 //create count div to display actual numbers
 const count_text = document.createElement('div');
 count_text.id = "count_text";
 counter.appendChild(count_text);
+//create title div to display counter title
+const title = document.createElement('div');
+title.id = "title";
+counter.appendChild(title);
+title.innerHTML = "confirmed global cases to date.";
 
 //get count:
 var total = script_tag.getAttribute("count");
 
 
 //counter div style:
-counter.style.border = "1px solid blue";
 counter.style.margin = "0 auto";
-counter.style["max-width"] = "400px";
+counter.style["max-width"] = "1000px";
+// counter.style["background-color"] = "#B0B8B4FF";
 
 //counter div text style:
-counter.style.fontSize = "200%";
 counter.style.textAlign = "center";
-title.style.color = "red";
+counter.style.color = "#184A45FF";
+// title.style.color = "#8E8D85";
+title.style.color="#FC766AFF";
+title.style.textAlign = "right";
+counter.style.fontSize = "200%";
 count_text.style.fontSize = "300%";
 var timer = 0;
 
